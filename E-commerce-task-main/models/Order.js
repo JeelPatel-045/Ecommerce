27d8212e93eb4a536e4f3601ce354f129887a3d1
Jeel Chaniyara <81005308+JeelPatel-045@users.noreply.github.com>
2025-10-23 22:34:32 +0530

    Delete E-commerce-task-main directory

--- E-commerce-task-main/models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  total_value: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Order', orderSchema);
